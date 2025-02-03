77466ae749ff8df556b0e48801fca1d98ca78b6a
Caesardi11 <ardicaesarkurniawan@gmail.com>
2025-02-03 15:32:16 +0700

    refactor styles for Collaboration and Footer components, enhance layout and responsiveness

--- components/Collaboration.jsx
+++ components/Collaboration.jsx
@@ -2,26 +2,24 @@ import React from 'react';
 
 const Collaboration = () => {
     return (
-        <div className=''>
-            <section className='collab-container '>
-                <img
-                    src="/kantor-pdka.png"
-                    alt="Background Image"
-                    className="collab-bg"
-                />
-                <div className="collab-bg-overlay"></div>
-                <div className="collab-content">
-                    <img src="/logo-pdka.png" alt="Logo" className="mb-3 h-12 lg:mb-6" />
-                    <h1 className="collab-heading">Engineering <span className='text-[#ffc800]'>Bureau</span> & General <span className='text-[#ffc800]'>Contractor</span></h1>
-                    <p className="collab-subheading">
-                        Hubungi kami dan mulailah perjalanan Anda dalam mewujudkan proyek impian berkualitas tinggi bersama tim ahli kami.
-                    </p>
-                    <div className="collab-btn">
-                        <a href="/contact"> Konsultasikan dengan kami → </a>
-                    </div>
+        <section className='collab-container '>
+            <img
+                src="/kantor-pdka.png"
+                alt="Background Image"
+                className="collab-bg"
+            />
+            <div className="collab-bg-overlay"></div>
+            <div className="collab-content">
+                <img src="/logo-pdka.png" alt="Logo" className="mb-3 h-12 lg:mb-6" />
+                <h1 className="collab-heading">Engineering <span className='text-[#ffc800]'>Bureau</span> & General <span className='text-[#ffc800]'>Contractor</span></h1>
+                <p className="collab-subheading">
+                    Hubungi kami dan mulailah perjalanan Anda dalam mewujudkan proyek impian berkualitas tinggi bersama tim ahli kami.
+                </p>
+                <div className="collab-btn">
+                    <a href="/contact"> Konsultasikan dengan kami → </a>
                 </div>
-            </section>
-        </div>
+            </div>
+        </section>
     );
 };
 
--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,81 @@
 import React from 'react'
+import { FaLocationDot } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className='footer-container'>
-        Footer
-    </div>
+    <section className='footer-container'>
+        <div className='footer-bg'></div>
+        <div className='footer-bg-overlay'></div>
+        <div className='relative z-10 mx-auto grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl 2xl:max-w-screen-2xl'>
+          <div className='border-b-2 border-gray-700'>
+            <a href="/" className='flex justify-center pb-5'>
+              <img src="/icon-navbar-2.png" alt="Logo PDKA" className='w-3/4'/>
+            </a>
+          </div>
+          <div className='text-warmWhite2 border-b-2 border-gray-700'>
+            <div>
+              <div className='flex flex-row gap-2 mb-5'>
+                <FaLocationDot className='mt-1 text-[#DAA520]'/>
+                <div>
+                  <h3 className='text-[#DAA520] font-semibold tracking-wide heading'>
+                    KANTOR PUSAT JAYAPURA
+                  </h3>
+                  <p className='mt-1 text-sm text-gray-400'>
+                    Jl. Ardipura, No. 56 A, <br /> Ardipura, Mandala, Jayapura Utara, <br /> Kota Jayapura, Papua 99222
+                  </p>
+                </div>
+              </div>
+              <div className='flex flex-row gap-2 mb-5'>
+                <FaLocationDot className='mt-1 text-[#DAA520]'/>
+                <div>
+                  <h3 className='text-[#DAA520] font-semibold tracking-wide heading'>
+                    KANTOR CABANG SORONG
+                  </h3>
+                  <p className='mt-1 text-sm text-gray-400'>
+                    Jl. A. Yani, Klademak III C, <br /> Klademak, Sorong Manoi, <br /> Kota Sorong, Papua Barat Daya 98414
+                  </p>
+                </div>
+              </div>
+              <div className='flex flex-row gap-2 mb-5'>
+                <FaLocationDot className='mt-1 text-[#DAA520]'/>
+                <div>
+                  <h3 className='text-[#DAA520] font-semibold tracking-wide heading'>
+                    KANTOR CABANG TIMIKA
+                  </h3>
+                  <p className='mt-1 text-sm text-gray-400'>
+                    Jl. Caritas SP 2 - SP 5, <br /> Timika Jaya, Mimika Baru, <br /> Kabupaten Mimika, Papua 99963
+                  </p>
+                </div>
+              </div>
+              <div className='flex flex-row gap-2 mb-5'>
+                <FaLocationDot className='mt-1 text-[#DAA520]'/>
+                <div>
+                  <h3 className='text-[#DAA520] font-semibold tracking-wide heading'>
+                    KANTOR CABANG MANOKWARI
+                  </h3>
+                  <p className='mt-1 text-sm text-gray-400'>
+                    Jl. Drs Esau Sesa (Samping Hotel Papua Forest), <br /> Wosi, Manokwari Barat <br /> Kabupaten Manokwari, Papua Barat 98312
+                  </p>
+                </div>
+              </div>
+              <div className='flex flex-row gap-2 mb-5'>
+                <FaLocationDot className='mt-1 text-[#DAA520]'/>
+                <div>
+                  <h3 className='text-[#DAA520] font-semibold tracking-wide heading'>
+                    KANTOR CABANG SURABAYA
+                  </h3>
+                  <p className='mt-1 text-sm text-gray-400'>
+                    Jl. Merak No.4 - 6, <br /> Krembangan Selatan, Krembangan, <br /> Surabaya, Jawa Timur 60175
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div>
+            asdasd
+          </div>
+        </div>
+    </section>
   )
 }
 
